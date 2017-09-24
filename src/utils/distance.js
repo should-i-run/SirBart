cc9c20acc44d7b3f4eb6e02cdd2f7943e80f1f15
Roger Goldfinger <rgoldfinger@gmail.com>
2017-09-24 14:10:22 -0700

    distance update

--- src/utils/distance.js
+++ src/utils/distance.js
@@ -1,25 +1,44 @@
 /* @flow */
 
-import type {Location} from '../actions/dataActions';
-import type {Station} from '../reducers/appStore';
+import type { Location } from '../actions/dataActions';
+import type { Station } from '../reducers/appStore';
 
 export function isSameLocation(old: Location, newLoc: Location) {
-  return (
-    old.lat === newLoc.lat &&
-    old.lng === newLoc.lng
-  );
+  return old.lat === newLoc.lat && old.lng === newLoc.lng;
 }
 
 export function getClosestEntrance(station: Station, start: ?Location) {
   if (start && station.entrances.length) {
     const getDistance = (entrance: Location, startLoc: Location) => {
-      const latDistance = Math.pow((startLoc.lat - entrance.lat), 2);
-      const lngDistance = Math.pow((startLoc.lng - entrance.lng), 2);
+      const latDistance = Math.pow(startLoc.lat - entrance.lat, 2);
+      const lngDistance = Math.pow(startLoc.lng - entrance.lng, 2);
       return Math.sqrt(lngDistance + latDistance);
     };
     // $FlowFixMe
-    const sortedEntrances = station.entrances.sort((a, b) => getDistance(a, start) - getDistance(b, start));
+    const sortedEntrances = station.entrances.sort(
+      (a, b) => getDistance(a, start) - getDistance(b, start),
+    );
     return sortedEntrances[0];
   }
-  return {lat: station.gtfs_latitude, lng: station.gtfs_longitude};
+  return { lat: station.gtfs_latitude, lng: station.gtfs_longitude };
+}
+
+// http://www.geodatasource.com/developers/javascript
+export function distanceBetweenCoordinates(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number,
+) {
+  const radlat1 = Math.PI * lat1 / 180;
+  const radlat2 = Math.PI * lat2 / 180;
+  const theta = lon1 - lon2;
+  const radtheta = Math.PI * theta / 180;
+  let dist =
+    Math.sin(radlat1) * Math.sin(radlat2) +
+    Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  dist = Math.acos(dist);
+  dist = dist * 180 / Math.PI;
+  dist = dist * 60 * 1.1515;
+  return dist * 1.609344;
 }
