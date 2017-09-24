2aa40507d4dadd0a990055c3ec7470cf67f0fd2b
Roger Goldfinger <rgoldfinger@gmail.com>
2017-09-24 14:10:22 -0700

    distance update

--- src/components/DataContainer.js
+++ src/components/DataContainer.js
@@ -1,20 +1,14 @@
 /* @flow */
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import React from 'react';
 
-import {
-  ScrollView,
-  Text,
-  RefreshControl,
-  View,
-  Platform,
-} from 'react-native';
+import { ScrollView, Text, RefreshControl, View, Platform } from 'react-native';
 
 import StationView from './Station';
 import Selector from './Selector';
 import DestinationSelector from './DestinationSelector';
-import {startLocation} from '../actions/locationActions';
+import { startLocation } from '../actions/locationActions';
 import {
   setupDataFetching,
   stopFetchingTimes,
@@ -24,15 +18,14 @@ import {
   refreshStations,
 } from '../actions/dataActions';
 
-import {
-  loadSavedDestinations,
-} from '../actions/destinationActions';
+import { loadSavedDestinations } from '../actions/destinationActions';
 
 import tracker from '../native/ga';
+import { distanceBetweenCoordinates } from 'utils/distance';
 
-import {colors} from '../styles';
+import { colors } from '../styles';
 
-import type {Station, Trip} from '../reducers/appStore';
+import type { Station, Trip } from '../reducers/appStore';
 
 type State = {
   fakeRefreshing: false,
@@ -41,12 +34,12 @@ type State = {
 type Props = {
   stations: ?Object,
   location: ?Object,
-  locationError: bool,
-  trips: ?Trip[],
+  locationError: boolean,
+  trips: ?(Trip[]),
   startLocation: Function,
   setupDataFetching: Function,
   fetchWalkingDirections: (s: Station) => void,
-  refreshingStations: bool,
+  refreshingStations: boolean,
   refreshStations: Function,
   fetchStations: Function,
   loadSavedDestinations: Function,
@@ -68,9 +61,21 @@ class DataContainer extends React.Component {
   }
 
   componentWillReceiveProps(nextProps: Props) {
+    const { location } = this.props;
     hackilySetLoc(nextProps.location);
     if (!this.props.location && nextProps.location) {
       this.props.fetchStations();
+    } else if (
+      location &&
+      nextProps.location &&
+      distanceBetweenCoordinates(
+        location.lat,
+        location.lng,
+        nextProps.location.lat,
+        nextProps.location.lng,
+      ) > 0.5
+    ) {
+      this.props.refreshStations();
     }
 
     if (nextProps.stations) {
@@ -84,38 +89,48 @@ class DataContainer extends React.Component {
 
   refreshStations = () => {
     this.props.refreshStations();
-    this.setState({fakeRefreshing: true});
+    this.setState({ fakeRefreshing: true });
     setTimeout(() => {
-      this.setState({fakeRefreshing: false});
+      this.setState({ fakeRefreshing: false });
     }, 800);
     tracker.trackEvent('interaction', 'pull-to-refresh');
-  }
+  };
 
   render() {
-    const {location, stations, locationError, trips} = this.props;
+    const { location, stations, locationError, trips } = this.props;
     return (
-      <View style={{flex: 1, backgroundColor: colors.background, paddingTop: Platform.select({android: 0, ios: 30})}}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: colors.background,
+          paddingTop: Platform.select({ android: 0, ios: 30 }),
+        }}
+      >
         <DestinationSelector />
         <ScrollView
           refreshControl={
             <RefreshControl
-              refreshing={this.props.refreshingStations || this.state.fakeRefreshing}
+              refreshing={
+                this.props.refreshingStations || this.state.fakeRefreshing
+              }
               onRefresh={this.refreshStations}
               tintColor="#E6E6E6"
             />
           }
         >
-          {stations && stations.map((s, i) => {
-            const tripForStation = trips && trips.find(l => l.code === s.abbr);
-            return (
-              <StationView
-                key={i}
-                station={s}
-                location={location}
-                tripForStation={tripForStation}
-              />
-            );
-          })}
+          {stations &&
+            stations.map((s, i) => {
+              const tripForStation =
+                trips && trips.find(l => l.code === s.abbr);
+              return (
+                <StationView
+                  key={i}
+                  station={s}
+                  location={location}
+                  tripForStation={tripForStation}
+                />
+              );
+            })}
           {locationError && <Text>Location Error</Text>}
         </ScrollView>
         <Selector />
@@ -132,14 +147,17 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = (dispatch: Function) =>
-  bindActionCreators({
-    startLocation,
-    setupDataFetching,
-    stopFetchingTimes,
-    fetchWalkingDirections,
-    refreshStations,
-    fetchStations,
-    loadSavedDestinations,
-  }, dispatch);
+  bindActionCreators(
+    {
+      startLocation,
+      setupDataFetching,
+      stopFetchingTimes,
+      fetchWalkingDirections,
+      refreshStations,
+      fetchStations,
+      loadSavedDestinations,
+    },
+    dispatch,
+  );
 
 export default connect(mapStateToProps, mapDispatchToProps)(DataContainer);
