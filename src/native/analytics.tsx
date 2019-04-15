0bf9cf1e3ea8c476a4dad723883ea17465d89014
Roger Goldfinger <rgoldfinger@gmail.com>
2019-04-14 22:43:07 -0700

    anaytics cleanup

--- src/native/analytics.tsx
+++ src/native/analytics.tsx
@@ -3,9 +3,9 @@ import Analytics from 'appcenter-analytics';
 // https://docs.microsoft.com/en-us/appcenter/sdk/analytics/react-native
 
 const wrapper = {
-  logEvent: (name: string) => {
+  logEvent: (name: string, otherArgs?: { [k: string]: string }) => {
     if (!__DEV__) {
-      Analytics.trackEvent(name);
+      Analytics.trackEvent(name, otherArgs);
       // Analytics.trackEvent('Video clicked', { Category: 'Music', FileName: 'favorite.avi' });
     }
   },
