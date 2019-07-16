eccea9eb7a8e1c914da750cb27fd19a0c0f3e8eb
rgoldfinger@gmail.com <rgoldfinger@gmail.com>
2019-07-15 22:02:25 -0700

    darker theme

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,9 +1,9 @@
 import { TextStyle } from 'react-native';
 
 export const colors = {
-  darkBackground: '#10131a',
-  background: '#19212C',
-  lightBackground: '#232E3D',
+  darkBackground: '#000',
+  background: '#10131a',
+  lightBackground: '#19212C',
   disabledText: '#999',
   walk: '#6FD57F',
   run: '#FC5B3F',
